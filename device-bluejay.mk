298dce2acdbd939e25a431c4df89fb66288550a8
flawedworld <flawedworld@flawed.world>
2022-07-28 17:47:37 +0100

    Add permissions and sysconfig from stock OS

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -59,6 +59,11 @@ $(call soong_config_set,lyric,camera_hardware,bluejay)
 $(call soong_config_set,lyric,tuning_product,bluejay)
 $(call soong_config_set,google3a_config,target_device,bluejay)
 
+# sysconfig and permissions XML from stock
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/product-sysconfig-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/product-sysconfig-stock.xml \
+	$(LOCAL_PATH)/product-permissions-stock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/product-permissions-stock.xml
+
 # Init files
 PRODUCT_COPY_FILES += \
 	device/google/bluejay/conf/init.blueport.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.blueport.rc \
